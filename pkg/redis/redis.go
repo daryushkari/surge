1d65fae6d2eca0cce316caf9f9e916c74590ed2f
mohammadreza kari <mohammadreza.k@780.ir>
2023-06-15 17:38:00 +0330

    add AddsortedSet function

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -1,6 +1,7 @@
-package redis
+package redisWrapper
 
 import (
+	"context"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -16,4 +17,13 @@ func InitClient(addr string) {
 
 func GetClient() *redis.Client {
 	return rdb
+}
+
+func AddSortedSet(ctx context.Context, key string, score float64, member interface{}) error {
+	z := redis.Z{
+		Score:  score,
+		Member: member,
+	}
+	_, err := rdb.ZAdd(ctx, key, z).Result()
+	return err
 }

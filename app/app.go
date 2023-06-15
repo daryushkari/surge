4f1490f67e1443b3ab5c9c7f488407d3975b0f7e
mohammadreza kari <mohammadreza.k@780.ir>
2023-06-15 10:08:20 +0330

    add ride router

--- app/app.go
+++ app/app.go
@@ -3,12 +3,22 @@ package app
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"surge/delivery"
 )
 
 func InitApp() {
 	r := gin.Default()
+	AddRideRouter(r)
 	err := r.Run("localhost:8080")
+
 	if err != nil {
 		log.Fatalln("error occurred:", err)
+	}
+}
+
+func AddRideRouter(r *gin.Engine) {
+	userRouter := r.Group("/ride")
+	{
+		userRouter.POST("/save", delivery.SaveRide)
 	}
 }

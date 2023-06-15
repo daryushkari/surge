8ba95981eb852a86b04e6213d996f25cef9adfc6
mohammadreza kari <mohammadreza.k@780.ir>
2023-06-15 22:20:39 +0330

    comment request-threshold table

--- entity/domain/requestThreshold.go
+++ entity/domain/requestThreshold.go
@@ -2,6 +2,9 @@ package domain
 
 import "gorm.io/gorm"
 
+// RequestThreshold has price coefficient for when requests pass predefined threshold
+// PriceCoefficient and RequestThreshold columns should be unique cause request threshold
+// should not have more than one coefficient and also coefficient must be unique and incremental
 type RequestThreshold struct {
 	gorm.Model
 	PriceCoefficient float64 `gorm:"unique"`

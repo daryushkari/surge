3f1ee3e489c0b53cd6c42709144608ec15502820
mohammadreza kari <mohammadreza.k@780.ir>
2023-06-16 21:53:56 +0330

    add test for is point in polygon function

---
+++ usecase/getDistrict_test.go
@@ -0,0 +1,34 @@
+package usecase
+
+import (
+	"surge/pkg/getPolygons"
+	"testing"
+)
+
+type isPointTests struct {
+	lat, lon float64
+	expected bool
+}
+
+var tableTests = []isPointTests{
+	isPointTests{5, 5, true},
+	isPointTests{-5, -5, false},
+	isPointTests{0, 0, false},
+}
+
+func TestIsPointInPolygon(t *testing.T) {
+	district := &getPolygons.DistrictPolygon{}
+	district.Points = []*getPolygons.Point{
+		{Latitude: 0, Longitude: 0},
+		{Latitude: 0, Longitude: 10},
+		{Latitude: 10, Longitude: -10},
+		{Latitude: 20, Longitude: 0},
+		{Latitude: 10, Longitude: 10},
+	}
+
+	for _, test := range tableTests {
+		if output := isPointInPolygon(test.lon, test.lat, district); output != test.expected {
+			t.Errorf("Output %t not equal to expected %t", output, test.expected)
+		}
+	}
+}

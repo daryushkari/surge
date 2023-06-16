b347b287c9062da44bf428b5e9ce9ff0206f9c88
mohammadreza kari <mohammadreza.k@780.ir>
2023-06-16 22:47:09 +0330

    remove redundant comments

--- main.go
+++ main.go
@@ -5,11 +5,3 @@ import "surge/app"
 func main() {
 	app.InitApp()
 }
-
-//rdb := redisWrapper.GetClient()
-//redisWrapper.AddSortedSet(context.Background(), "u1", float64(time.Now().UnixMilli()), float64(time.Now().UnixMilli()))
-//res, _ := redisWrapper.GetCount(context.Background(), "u1",
-//fmt.Sprintf("%d", time.Now().UnixMilli()-60*1000*30), fmt.Sprintf("%d", time.Now().UnixMilli()))
-//redisWrapper.RemoveOldElements(context.Background(), "u1",
-//fmt.Sprintf("%d", time.Now().UnixMilli()-60*1000*30))
-//log.Println("counts is ", res)
